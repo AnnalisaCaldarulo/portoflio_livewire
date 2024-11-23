6a5d8e2ad1c0c8083474653aa74b6d6a281b2ae6
Annalisa Caldarulo <annalisacaldarulo97@gmail.com>
2024-11-23 11:02:16 +0100

    table style

--- resources/views/livewire/element-list.blade.php
+++ resources/views/livewire/element-list.blade.php
@@ -1,5 +1,5 @@
-<div>
-    <div class="grid sm:grid-cols-12 gap-4 mt-4">
+<div class="grid sm:grid-cols-12">
+    <div class=" col-span-12 grid sm:grid-cols-12 gap-4 mt-4">
         <div class="col-span-12 grid sm:grid-cols-12 gap-4 mb-5">
 
             <h2 class="text-2xl text-amber-500 sm:col-span-2 font-bold hover:cursor-pointer capitalize"> <i
@@ -25,25 +25,31 @@
         </div>
     </div>
 
-    <table class="p-4 table-auto text-white border ">
-        <thead>
-            <tr>
-                @foreach ($cols as $col)
-                    <th>{{ $col }}</th>
-                @endforeach
-
-            </tr>
-        </thead>
-        @dump($cols)
-        <tbody>
-            @foreach($elementList as $element)
-            <tr>
-                @foreach($element->getFillable() as $attribute)
-                <td>{{$element->$attribute}} </td>
-                @endforeach
-            </tr>
-           @endforeach
-        </tbody>
-    </table>
+    <div class="col-span-12 sm:col-span-8 sm:col-start-3">
+        <table class="border-separate border border-white w-full table-auto text-white  rounded-lg">
+            <thead>
+                <tr class="bg-slate-900">
+                    @foreach ($cols as $col)
+                        <th class="border border-slate-300">{{ $col }}</th>
+                    @endforeach
+                    <th class="border border-slate-300">actions</th>
+    
+                </tr>
+            </thead>
+            <tbody class="" >
+                @foreach($elementList as $element)
+                <tr class="">
+                    @foreach($element->getFillable() as $attribute)
+                    <td class="text-center p-5 border border-slate-300">{{$element->$attribute}} </td>
+                    @endforeach
+                    <td class="text-center  flex justify-center gap-4 items-center border border-slate-300 p-5 ">
+                        <button class="bg-yellow-500 h-100 w-1/4 rounded p-3">Ciao</button>
+                        <button class="bg-red-500 w-1/4 rounded p-3">Ciao</button>
+                    </td>
+                </tr>
+               @endforeach
+            </tbody>
+        </table>
+    </div>
 
 </div>
